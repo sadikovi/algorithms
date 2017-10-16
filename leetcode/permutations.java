9d154d0d9fdbe0de3c0970e5dc204eeaed13d9fc
sadikovi <ivan.sadikov@gmail.com>
2017-10-17 08:03:11 +1300

    add permutations solution

--- leetcode/permutations.java
+++ leetcode/permutations.java
@@ -1,4 +1,4 @@
-// Permutation problem, similar to permutation of strings. Runs in O(2^n), where n is array length,
+// Permutation problem, similar to permutation of strings. Runs in O(n!), where n is array length,
 // similar the one for strings.
 // The main gotcha is avoiding array copy for each iteration, that is why we propagate indices
 // 'start' and 'end' for each recursion.
@@ -22,6 +22,34 @@ public class Solution {
           ext.add(nums[start]);
           for (int p = i; p < arr.size(); p++) ext.add(arr.get(p));
           res.add(ext);
+        }
+      }
+    }
+    return res;
+  }
+}
+
+// Slightly faster solution (based on Leetcode runtime distribution, beats 67.76%)
+// We use only one index to indicate current position, everything to the left has already been
+// permuted, to the right - yet to be processed.
+class Solution {
+  public List<List<Integer>> permute(int[] nums) {
+    return permute(nums, nums.length - 1);
+  }
+
+  private List<List<Integer>> permute(int[] nums, int pos) {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    if (pos == 0 && pos < nums.length) {
+      List<Integer> tmp = new ArrayList<Integer>();
+      tmp.add(nums[pos]);
+      res.add(tmp);
+    } else if (pos < nums.length) {
+      List<List<Integer>> arr = permute(nums, pos - 1);
+      for (List<Integer> t : arr) {
+        for (int i = 0; i <= t.size(); i++) {
+          List<Integer> cp = new ArrayList<Integer>(t);
+          cp.add(i, nums[pos]);
+          res.add(cp);
         }
       }
     }
