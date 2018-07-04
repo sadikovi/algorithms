2bdc8d588214350ebc2fb3e3ac5fe1d9b11c2590
Ivan Sadikov <ivan.sadikov@gmail.com>
2018-07-04 19:56:11 +0200

    add solution

--- leetcode/powx-n.java
+++ leetcode/powx-n.java
@@ -1,4 +1,4 @@
-// Solution runs in O(log n) time and O(log n) space, because it halves search space in half for
+// Solution runs in O(log n) time and O(log n) space, because it halves search space for
 // every recursive call.
 class Solution {
   public double myPow(double x, int n) {
@@ -12,5 +12,21 @@ class Solution {
     if (n == 0) return 1;
     double res = pow(x, n / 2);
     return (n % 2 == 0) ? (res * res) : (res * res * x);
+  }
+}
+
+// Alternative solution
+// Runs in O(log n) time and O(log n) space, beats 100% of submissions.
+class Solution {
+  public double myPow(double x, int n) {
+    return n < 0 ? pow(1/x, -((long) n)) : pow(x, (long) n);
+  }
+
+  private double pow(double x, long n) {
+    if (n == 0) return 1;
+    if (n == 1) return x;
+    long rem = n % 2;
+    double res = pow(x, n /2);
+    return rem == 0 ? res * res : res * res * x;
   }
 }
