5bfdbd115db75ee192f201cee3063b7bd2c77baa
Ivan Sadikov <ivan.sadikov@gmail.com>
2019-03-28 09:33:59 +0100

    update solution

--- leetcode/product-of-array-except-self.java
+++ leetcode/product-of-array-except-self.java
@@ -18,28 +18,21 @@
  * See steps code for explanation how we come to the solution below.
  */
 public class Solution {
-  // final solution, runs in O(1) space complexity and O(n) time complexity
+  // final fast solution, runs in O(1) space complexity (without `out`) and O(n) time complexity
+  // works on empty arrays and arrays of 1 element.
   public int[] productExceptSelf(int[] nums) {
-    int n = nums.length;
-    int[] res = new int[n];
-
-    // Bulding rolling product left -> right
-    int product = 1;
-    for (int i = 0; i < n; i++) {
-      res[i] = product * nums[i];
-      product *= nums[i];
+    int[] out = new int[nums.length];
+    int p = 1; // assume product does not overflow
+    for (int i = 0; i < nums.length; i++) { // Bulding rolling product left -> right
+      p *= nums[i];
+      out[i] = p;
     }
-
-    // Going in opposite direction and using previous element in `res` as left[i - 1]
-    // and `product` as right[i + 1]
-    product = 1;
-    for (int i = n - 1; i > 0; i--) {
-      res[i] = res[i - 1] * product;
-      product *= nums[i];
+    p = 1; // reset to use from right to left
+    for (int i = nums.length - 1; i >= 0; i--) {
+      out[i] = i == 0 ? p : (out[i - 1] * p);
+      p *= nums[i];
     }
-    res[0] = product;
-
-    return res;
+    return out;
   }
 }
 
