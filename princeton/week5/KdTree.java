e1acd6336a8dd3cf909360dc3d21361b97e6d2b9
Ivan Sadikov <ivan.sadikov@gmail.com>
2020-06-17 22:10:20 +0200

    add week5

---
+++ princeton/week5/KdTree.java
@@ -0,0 +1,49 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class KdTree {
+  // construct an empty set of points
+  public KdTree() {
+
+  }
+
+  // is the set empty?
+  public boolean isEmpty() {
+    throw new RuntimeException();
+  }
+
+  // number of points in the set
+  public int size() {
+    throw new RuntimeException();
+  }
+
+  // add the point to the set (if it is not already in the set)
+  public void insert(Point2D p) {
+    throw new RuntimeException();
+  }
+
+  // does the set contain point p?
+  public boolean contains(Point2D p) {
+    throw new RuntimeException();
+  }
+
+  // draw all points to standard draw
+  public void draw() {
+    throw new RuntimeException();
+  }
+
+  // all points that are inside the rectangle (or on the boundary)
+  public Iterable<Point2D> range(RectHV rect) {
+    throw new RuntimeException();
+  }
+
+  // a nearest neighbor in the set to point p; null if the set is empty
+  public Point2D nearest(Point2D p) {
+    throw new RuntimeException();
+  }
+
+  // unit testing of the methods (optional)
+  public static void main(String[] args) {
+    throw new RuntimeException();
+  }
+}
