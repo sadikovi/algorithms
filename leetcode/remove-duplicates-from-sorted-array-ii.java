996d1230e1967364bdea2cd1e921485196ee0ec3
Ivan Sadikov <ivan.sadikov@gmail.com>
2020-07-08 20:39:45 +0200

    update solution

--- leetcode/remove-duplicates-from-sorted-array-ii.java
+++ leetcode/remove-duplicates-from-sorted-array-ii.java
@@ -1,3 +1,17 @@
+// Solution runs in O(n) time and updates the input array in-place.
+// Space complexity is O(1).
+//
+// Test cases:
+//
+// [1,1,1,2,2,3]
+// [0,0,1,1,1,1,2,3,3]
+// [0]
+// [0,0]
+// [0,1]
+// [0,1,2,3,4,5,6]
+// [1,1,1,1,1,1,2,3,4,4]
+// [1,1,1,1,1]
+
 public class Solution {
   public int removeDuplicates(int[] nums) {
     if (nums == null || nums.length == 0) return 0;
@@ -13,5 +27,23 @@ public class Solution {
       }
     }
     return ++index;
+  }
+}
+
+// Another solution, similar logic, keep in mind that we update the counters after the check.
+class Solution {
+  public int removeDuplicates(int[] nums) {
+    int idx = 0, dup = 0;
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] != nums[idx]) {
+        nums[++idx] = nums[i];
+        dup = 0;
+      } else if (nums[i] == nums[idx] && dup < 1) {
+        nums[++idx] = nums[i];
+        dup++;
+      }
+    }
+
+    return idx + 1;
   }
 }
